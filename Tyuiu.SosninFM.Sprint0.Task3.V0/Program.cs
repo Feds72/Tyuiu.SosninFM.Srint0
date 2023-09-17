457119935e89129202da038a181be2340643e17a
федор соснин <sosninfedor@gmail.com>
2023-09-17 19:33:39 +0500

    Task4

--- Tyuiu.SosninFM.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.SosninFM.Sprint0.Task3.V0/Program.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.SosninFM.Sprint0.Task3.V0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.Sum(7, 7));
+            Console.WriteLine(DataService.Sum(7, 21));
             Console.ReadKey();
         }
     }
---
+++ Tyuiu.SosninFM.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SosninFM.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.SosninFM.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // сложение
+            Console.WriteLine(DataService.Addition(1, 4353));
+            // вычитание
+            Console.WriteLine(DataService.Substraction(226, 2));
+            // умножение
+            Console.WriteLine(DataService.Multiplication(3, 4));
+            // деление
+            Console.WriteLine(DataService.Division(25, 5));
+
+            Console.ReadKey();
+
+        }
+    }
+}
